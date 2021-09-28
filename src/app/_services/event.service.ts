23ab3e8ccae8e41a405e04796710c856eb594eca
Álvaro Pérez García <aperez@joor.com>
2021-09-28 02:22:18 +0200

    Add env variables

--- src/app/_services/event.service.ts
+++ src/app/_services/event.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const API_URL = 'http://localhost:3081/api/';
+const API_URL = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
